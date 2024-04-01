2ce81145c4d0800d59651b61ca835ce9c3c8006c
nguyenvantrinh77 <107011525+nguyenvantrinh77@users.noreply.github.com>
2024-04-01 08:35:51 +0700

    Add files via upload

---
+++ bai2.cpp
@@ -0,0 +1,33 @@
+﻿#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+
+// Hàm kiểm tra xem một số có phải là số chính phương hay không
+int isPerfectSquare(int num) {
+	int sqrt_num = sqrt(num);
+	return sqrt_num * sqrt_num == num;
+}
+
+// Hàm đếm và in ra các số chính phương nhỏ hơn n
+void countAndPrintPerfectSquares(int n) {
+	printf("Cac so chinh phuong nho hon %d la:\n", n);
+	for (int i = 1; i < n; ++i) {
+		if (isPerfectSquare(i)) {
+			printf("%d ", i);
+		}
+	}
+}
+
+int main() {
+	int n;
+	srand(time(NULL)); // Khởi tạo seed cho hàm random
+	n = rand() % 100 + 1; // Tạo số nguyên dương ngẫu nhiên từ 1 đến 100
+
+	printf("So nguyen duong n duoc tao ngau nhien la: %d\n", n);
+
+	// Gọi hàm để đếm và in ra các số chính phương nhỏ hơn n
+	countAndPrintPerfectSquares(n);
+
+	return 0;
+}
